73ad1bb1d99d1008f9e3b3bba285158ca2ea6590
Kishan <kumarkishan2991@gmail.com>
2023-06-12 22:56:19 +0530

    operators POC is done

--- src/main/java/com/demo/operators/BitDemo.java
+++ src/main/java/com/demo/operators/BitDemo.java
@@ -5,11 +5,18 @@ public class BitDemo {
 	public static void main(String[] args) {
          int bitmask =0x00F;
          int val =0x2222;
-         
+        
+         System.out.println(bitmask);
+         System.out.println(val);
+        
          //Prints "2"
          
          System.out.println(val & bitmask);
+		System.out.println(~bitmask);
+		System.out.println(bitmask<<3);
+		System.out.println(bitmask>>4);
 		
+		System.out.println(bitmask>>>4);
 	}
 
 }
