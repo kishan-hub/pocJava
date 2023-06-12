7669f98f89fa92cc9a957df556f3f3133618b9cb
Kishan <kumarkishan2991@gmail.com>
2023-06-12 15:05:59 +0530

    poc of uses of operators in java

---
+++ src/main/java/com/demo/var/User.java
@@ -0,0 +1,21 @@
+package com.demo.var;
+
+public class User {
+	
+//	private var name ="Sure";
+//	public void setName(var name) {
+//		this.name =name;
+//	}
+	//above field parameter wriiten not possible because compiler not recognize the var variable
+	
+//	
+//	public String greetings(int message) {
+//		greetings=null;
+//		
+//		if(message ==0) {
+//		  	
+//		}
+//	}
+//	
+
+}
--- src/main/java/com/demo/var/VarDemoAnalysis.java
+++ src/main/java/com/demo/var/VarDemoAnalysis.java
@@ -1,10 +1,6 @@
 package com.demo.var;
-
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
+
 import java.util.List;
 
 public class VarDemoAnalysis {
